4f337b17fa4d1b94f8031363b4b764a15be8bfe8
LingKa <cnfty786@gmail.com>
2023-11-28 11:11:56 +0800

    test: update business clients
    Signed-off-by: LingKa <cnfty786@gmail.com>

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -24,7 +24,7 @@ import (
 )
 
 func TestAuthUser(t *testing.T) {
-	xlog.SetLevel(zapcore.WarnLevel)
+	xlog.SetLevel(zapcore.ErrorLevel)
 
 	curpMembers := []string{"172.20.0.3:2379", "172.20.0.4:2379", "172.20.0.5:2379"}
 
